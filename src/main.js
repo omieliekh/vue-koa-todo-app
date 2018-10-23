23d1d4e1d724e66bd4bd0d30c76770baa5aafc29
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2018-10-23 22:42:07 +0800

    Add Auth service

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import VueResource from 'vue-resource';
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Auth from './services/Auth'
 
 Vue.use(VueResource);
+Vue.http.interceptors.push(Auth.authHeaderInterceptor);
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ src/services/Auth.js
@@ -0,0 +1,62 @@
+import Vue from 'vue'
+import router from '../router'
+
+export default {
+  user: null,
+
+  _setUser: function (data) {
+    localStorage.setItem('auth_token', data.token);
+    this.user = data.user;
+    router.push('/');
+  },
+
+  _unsetUser: function () {
+    localStorage.removeItem('auth_token');
+    this.user = null;
+    router.push('login');
+  },
+
+  login (creds) {
+    return Vue.http.post('/api/login', creds)
+      .then(this._setUser);
+  },
+
+  signup (userData) {
+    return Vue.http.post('/api/sign-up', userData)
+      .then(this._setUser);
+  },
+
+  logout () {
+    this._unsetUser();
+  },
+
+  checkAuth () {
+    const token = localStorage.getItem('auth_token');
+
+    if (!token) {
+      this._unsetUser();
+      return Promise.resolve(null);
+    }
+
+    return Vue.http.post('/api/check-auth', { token })
+      .then(({ data }) => {
+        if (data.user) {
+          this.user = data.user
+        } else {
+          this.user = null
+        }
+
+        return data
+      })
+  },
+
+  authHeaderInterceptor (request, next) {
+    const token = localStorage.getItem('auth_token');
+
+    if (token) {
+      request.headers.set('Authorization', 'Bearer ' + localStorage.getItem('auth_token'));
+    }
+
+    next();
+  }
+}
