775e2879ace9f09e5185eb68d5a4e52d31ef0251
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2018-10-23 22:16:50 +0800

    Add vue-resource

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource';
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+Vue.use(VueResource);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
