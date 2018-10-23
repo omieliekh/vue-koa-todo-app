a60237c5166786dcdda00f7129980a9a23404fd5
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2018-10-23 22:53:10 +0800

    Add bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource';
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import Auth from './services/Auth'
 
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 Vue.use(VueResource);
 Vue.http.interceptors.push(Auth.authHeaderInterceptor);
+Vue.use(BootstrapVue);
+
 Vue.config.productionTip = false;
 
 new Vue({
