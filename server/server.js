569007fcc2e80993bf74d3fb7c861b1f9418276d
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2018-10-23 23:00:45 +0800

    Add koa-body parser

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 /* eslint-disable no-console */
 const Koa = require('koa');
+const bodyParser = require('koa-body');
 const app = new Koa();
 
 const encodeDecode = require('./encode-decode');
 const auth = require('./auth');
+
+app.use(bodyParser({
+  formidable:{
+    uploadDir: './uploads'
+  },
+  multipart: true,
+  urlencoded: true
+}));
 
 app.use(encodeDecode());
 app.use(auth.routes());
