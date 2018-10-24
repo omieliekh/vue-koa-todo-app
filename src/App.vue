0fd1043af4a2dc44c993ad81aca1946b2f2358ba
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2018-10-24 22:05:30 +0800

    Remove unused components

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="login">Login</router-link> |
-      <router-link to="/todos">Todos</router-link> |
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Todos</router-link>
     </div>
     <router-view/>
   </div>
--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
 import Todos from '@/components/Todos'
-import Home from './views/Home.vue'
 
 Vue.use(Router);
 
@@ -16,22 +15,9 @@ export default new Router({
       component: Login
     },
     {
-      path: '/todos',
+      path: '/',
       name: 'todos',
       component: Todos
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
