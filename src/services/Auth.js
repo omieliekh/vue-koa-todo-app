e6d11603a98e44088d58a5b6903636a8349f03c1
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2018-10-24 21:59:54 +0800

    Fix auth token storing on FE

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -18,12 +18,12 @@ export default {
 
   login (creds) {
     return Vue.http.post('/api/login', creds)
-      .then(this._setUser.bind(this));
+      .then(res => this._setUser(res.body));
   },
 
   signup (userData) {
     return Vue.http.post('/api/sign-up', userData)
-      .then(this._setUser.bind(this));
+      .then(res => this._setUser(res.body));
   },
 
   logout () {
