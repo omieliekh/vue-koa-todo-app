213677a6b12163d97bef9db9c9f56e54fb5e53e1
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2018-10-25 13:43:48 +0800

    Setup unit tests with karma and mocha

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  rules: {
+    semi: 2
+  }
+}
---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpackConfig = require(path.join(process.cwd(), '/node_modules/@vue/cli-service/webpack.config.js'));
+
+
+module.exports = function (config) {
+  config.set({
+    frameworks: ['mocha'],
+
+    files: [
+      'src/**/*.spec.js'
+    ],
+
+    preprocessors: {
+      '**/*.spec.js': ['webpack', 'sourcemap']
+    },
+
+    webpack: webpackConfig,
+
+    reporters: ['spec'],
+
+    browsers: ['Chrome']
+  });
+};
