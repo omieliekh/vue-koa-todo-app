31f10d084cbc03449b25490faa4eb4cb16345d13
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2018-10-25 15:10:03 +0800

    Fix lint errors in tests

--- test/test.utils.js
+++ test/test.utils.js
@@ -1,4 +1,4 @@
-import { expect, assert } from 'chai';
+import { assert } from 'chai';
 
 export function tick() {
   return new Promise(resolve => setTimeout(resolve));
@@ -9,5 +9,5 @@ export function elementExists(wrapper, selector) {
 }
 
 export function elementNotExists(wrapper, selector) {
-  assert(!wrapper.find(selector).exists(), `'${selector}' element exists, but it shouldn\'t`);
+  assert(!wrapper.find(selector).exists(), `'${selector}' element exists, but it shouldn't`);
 }
